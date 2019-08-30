d0310f191d0a806fd97719d65b2f1c430fce4f85
Freddy12D <54275776+Freddy12D@users.noreply.github.com>
2019-08-29 23:10:18 -0500

    Update ejercicio6.cpp

--- ejercicio6.cpp
+++ ejercicio6.cpp
@@ -4,3 +4,16 @@
  Y encuentra el mayor valor: 30
 
  */
+#include <iostream>
+using namespace std;
+int main() {
+    int n=3, j, max=0;
+    for (int i=0; i<n; i++){
+        cin>> j;
+        if (max<j||j==max) {
+            max = j;
+        }
+    }
+    cout<<max;
+    return 0;
+}
